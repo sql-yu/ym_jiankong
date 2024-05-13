350de8dd6e8b93a619857879c7e5c5815c6b9ff8
sql-yu <296219316@qq.com>
2024-05-13 17:35:51 +0800

    优化翻译

--- resources/lang/zh_CN/ym-package.php
+++ resources/lang/zh_CN/ym-package.php
@@ -5,7 +5,8 @@ return [
         'ym-package' => '包列表',
     ],
     'fields' => [
-        'package_name' => 'package_name',
+        'app_name'=>'包名',
+        'package_name' => '包id',
         'review_time' => '提审时间',
         'pass_time' => '通过时间',
         'takedown_time' => '下架时间',
